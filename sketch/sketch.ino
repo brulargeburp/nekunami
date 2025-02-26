f993d07ee50dc92acee5ba656d183a17ac504978
brulargeburp <bonhayagfabien@gmail.com>
2025-02-26 14:07:46 +0800

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,7 +1,7 @@
 int dc_sensorPin = A0; // Analog pin connected to the voltage sensor
 int ac_sensorPin = A1; // Voltage sensor for the load
-int relayPin = 7;   // Digital pin connected to the relay (simulating circuit breaker)
-float voltageThreshold = 4.5; // Voltage threshold to trip the circuit (adjust as needed)
+int relayPin = 7;   // Digital pin connected to the relay (simulating circuit breaker) PLaceholder
+float voltageThreshold = 4.5; // Voltage threshold to trip the circuit (adjust as needed) Placeholder
 bool circuitTripped = false; // Flag to indicate if the circuit has been tripped
 
 void setup() {
@@ -25,22 +25,10 @@ void loop() {
   Serial.print("Measured AC Voltage: ");
   Serial.println(ac_voltage);
 
-  // SSR Circuit Breaker Auto Trip Logic
-  if (voltage > voltageThreshold && !circuitTripped) {
-    Serial.println("Overvoltage detected! Tripping circuit.");
-    digitalWrite(relayPin, LOW); // Open the relay (trip the circuit)
-    circuitTripped = true;      // Set the flag so it doesn't keep tripping
-    Serial.println("Circuit tripped.");
-  }
-  else if (voltage <= voltageThreshold && circuitTripped)
-  {
-    Serial.println("Voltage normalized! Circuit would still require reset.");
-  }
-
   // SSR Circuit Breaker Manual Trip Logic
   if(Serial.available() > 0)
   {
-    Incoming_value = Serial.read();
+    int Incoming_value = Serial.read();
     Serial.print(Incoming_value);
     Serial.print("\n");
     if(Incoming_value == '1')
